896a96b5d57d229fc63330d6eb11cb06f206f597
lauro <laurolyra@gmail.com>
2020-08-03 01:19:43 -0300

    installing packages and setting enviroment variables

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -16,7 +17,9 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const appPort = process.env.PORT || 3001;
+
+const hereIsTheUpsideDown = process.env.upsideDown;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +30,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
-});
+app.listen(appPort, () => {
+  console.log(`Escutando na porta ${appPort} e UpsideDown = ${hereIsTheUpsideDown}`);
+})
